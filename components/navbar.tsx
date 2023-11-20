85a657ff090a66f55da43c6d1d0e9674c168192d
Rageshwaran-HR <118850069+Rageshwaran-HR@users.noreply.github.com>
2023-11-21 02:24:57 +0530

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,41 +1,57 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import { Menu,Sparkles} from "lucide-react"; // It seems you're using an external library named "lucide-react" for icons.
+import { Menu, Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
-import Link from "next/link"; // Importing the Link component from Next.js for client-side navigation.
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MobileSlidebar } from "./mobile-slidebar";
 
-const font =Poppins({
-    weight: "600",
-    subsets: ["latin"],
-})
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+    <motion.div
+      initial={{ opacity: 0, y: -30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16"
+    >
       <div className="flex items-center">
-        <MobileSlidebar/>
+        <MobileSlidebar />
         <Link href="/">
-          <h1 className={cn(
-            "hidden md:block text-xl md:text-3xl font-bold text-green-600 ",
-            font.className
-            )}>
+          <motion.h1
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary text-green-600",
+              font.className
+            )}
+          >
             AI-Character
-          </h1>
+          </motion.h1>
         </Link>
       </div>
-      <div className="flex items-center gap-x-3">
+      <motion.div
+        initial={{ opacity: 0, x: 30 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        className="flex items-center gap-x-3"
+      >
         <Button variant="premium" size="sm">
-            upgrade
-        <Sparkles className="h-4 fill-white text-white ml-2" />
+          upgrade
+          <Sparkles className="h-4 fill-white text-white ml-2" />
         </Button>
-        <ModeToggle/>
-        <UserButton afterSignOutUrl="/"/>
-      </div>
-    </div>
+        <ModeToggle />
+        <UserButton />
+      </motion.div>
+    </motion.div>
   );
-}
+};
